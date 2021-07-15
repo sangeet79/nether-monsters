60d2562d37275ad6531e65dede1d4003c186863f
sangeet79 <icef@mail.ru>
2021-07-16 00:47:06 +0300

    Update init.lua
    Applied changes, suggested by cure (https://github.com/cure)

--- init.lua
+++ init.lua
@@ -1,12 +1,24 @@
 local modname = minetest.get_current_modname()
 local modpath = minetest.get_modpath(modname)
 
--- Nether Man # 129 code lines
+nethermobs                   = {}
+nethermobs.NETHERMAN_ENABLED = true
+nethermobs.DRAGON_ENABLED    = true
 
-dofile(modpath.."/netherman.lua")
+-- Override default settings with values from the .conf file, if any are present.
+nethermobs.NETHERMAN_ENABLED       = minetest.settings:get_bool("nethermobs.netherman_enabled", nethermobs.NETHERMAN_ENABLED)
+nethermobs.DRAGON_ENABLED          = minetest.settings:get_bool("nethermobs.dragon_enabled", nethermobs.DRAGON_ENABLED)
 
--- Nether Dragon # 657 code lines
+minetest.log("action", "[MOD] Nethermobs loaded")
+minetest.log("info", "[MOD] Nethermobs: nethermobs.NETHERMAN_ENABLED: "..tostring(nethermobs.NETHERMAN_ENABLED))
+minetest.log("info", "[MOD] Nethermobs: nethermobs.DRAGON_ENABLED: "..tostring(nethermobs.DRAGON_ENABLED))
 
-dofile(modpath.."/dragon.lua")
+if nethermobs.NETHERMAN_ENABLED then
+        -- Nether Man # 129 code lines
+        dofile(modpath.."/netherman.lua")
+end
 
--- please read README.md
+if nethermobs.DRAGON_ENABLED then
+        -- Nether Dragon # 657 code lines
+        dofile(modpath.."/dragon.lua")
+end
